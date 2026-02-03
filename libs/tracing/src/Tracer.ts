e814d70ffb95c3173764c06196f79a6aa53fd88e
Kesin Ryan Dehejia <imkesin@gmail.com>
2026-02-03 12:25:31 -0800

    feat: add `tracing` package

---
+++ libs/tracing/src/Tracer.ts
@@ -0,0 +1,40 @@
+import * as OtlpTracer from "@effect/opentelemetry/OtlpTracer"
+import * as NodeHttpClient from "@effect/platform-node/NodeHttpClient"
+import * as Config from "effect/Config"
+import * as Effect from "effect/Effect"
+import { pipe } from "effect/Function"
+import * as Layer from "effect/Layer"
+import * as Redacted from "effect/Redacted"
+
+const HoneycombConfig = Config.nested(
+  Config.all({
+    apiKey: pipe(
+      Config.string("API_KEY"),
+      (_) => Config.redacted(_)
+    ),
+    dataset: pipe(
+      Config.string("DATASET"),
+      Config.withDefault("effect-workos.integration")
+    )
+  }),
+  "HONEYCOMB"
+)
+
+export const TracerLive = Layer.unwrapEffect(
+  Effect.gen(function*() {
+    const honeycombConfig = yield* HoneycombConfig
+
+    return OtlpTracer.layer({
+      url: "https://api.honeycomb.io/v1/traces",
+      resource: {
+        serviceName: honeycombConfig.dataset
+      },
+      headers: {
+        "x-honeycomb-team": Redacted.value(honeycombConfig.apiKey),
+        "x-honeycomb-dataset": honeycombConfig.dataset
+      }
+    })
+  })
+).pipe(
+  Layer.provide(NodeHttpClient.layerUndici)
+)
